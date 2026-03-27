1a5c50e61251aa3f8a87a9b6998d9e077a9eea63
Divyanshu <107361326+DIVDRIP@users.noreply.github.com>
2026-03-28 00:19:41 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://orderflow-2dfi.onrender.com",
 });
 
 export const getOrders = (params) =>
